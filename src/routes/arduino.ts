d838c0cd9b08ac845509750cfa9ae3286b4883f7
ramondelemos <ramondelemos@gmail.com>
2018-04-08 22:04:33 -0300

    Configuração dos comandos para o led na porta 13.

--- src/routes/arduino.ts
+++ src/routes/arduino.ts
@@ -1,20 +1,36 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import * as five from "johnny-five";
 
 class Arduino {
-    
-    router: Router
-    
+
+    public router: Router;
+    public board: five.Board;
+
     constructor() {
         this.router = Router();
         this.init();
     }
 
     private init() {
+        this.board = new five.Board({ port: "/dev/ttyUSB0" });
         this.router.get('/', this.get);
     }
+
+    public get(req: Request, res: Response, next: NextFunction) {        
+
+            let turn = req.query.turn;
+            
+            let led: five.Led;
+            led = new five.Led(13);
+
+            if (turn == "on") {
+                led.on();
+            } else {
+                led.off();
+            }
     
-    public get(req: Request, res: Response, next: NextFunction) {
-        res.send("Hello World!");
+            res.send({ result: turn });
+
     }
 
 }
